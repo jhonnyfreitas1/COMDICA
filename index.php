806e3baa530539214bffeb1767a5e6f56c0f0c35
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-04-10 22:27:56 -0300

    mudanças na pagina principal para ser mas atrativo com possiveis mudanças em um futuro proximo

--- index.php
+++ index.php
@@ -1,165 +1,171 @@
 <!doctype html>
 <html lang="en">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="assets/css/index-css.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/index-css.css">
 
-  <title>COMDICA - Conselho municipal dos direitos da criança e do adolescente.</title>
-</head>
+    <title>COMDICA - Conselho municipal dos direitos da criança e do adolescente.</title>
+  </head>
 
 <?php 
-    include 'nav.php';
+include 'nav.php';
 ?>  
 
 <body>
-   <div class="section">
-    <img src="assets/img/p4.png" class="img-fluid rounded" alt="Responsive image" style="margin-top:30em; height: 40em; ">
-        <div class="container mb-8  col-md-8" id="card-fundo" style="">
-          <div class="row">
-            <div class=" col-md-12  text-dark position-top ">
-              <h2 class="title">FUMDECA</h2>
-              <h5 class="text-muted">
-                O Fundo Municipal dos Direitos da Criança e do Adolescente (FUMDECA) é um Fundo especial, criado por lei municipal, segundo determinação do Estatuto da Criança e do Adolescente em seu art. 260 com o objetivo de financiar programas e projetos que atuem na garantia da promoção, proteção e defesa dos direitos da criança e do adolescente. 
-              </h5>
-              <!-- <div class="nucleo-container">
-              <img src="assets/img/nucleo.svg" alt="">
-            </div> -->
 
-              <a href="nucleo-icons.html" class="btn btn-info" target="_blank">Saiba mas</a>
+ 
+<div style="margin-top:20em; padding-bottom:6em;"> </div>
+  <div class="section">
+
+    <div class="row"> 
+     <div id="logo-icon" class="row col-md-6 col-12 mr-12 p-10">
+      <img src="assets/img/mc2.png" class="img-fluid rounded col-10 m-6" alt="Responsive image">
+    </div>
+
+    <div class="container float-right col-md-5 p-12" id="card-fundo" style="background-color: #F6A4B4;">
+      <div class="row">
+        <div class=" col-md-12 text-dark position-top">
+          <h2 class="title">FUMDECA</h2>
+          <h5 class="text-muted">
+            O Fundo Municipal dos Direitos da Criança e do Adolescente (FUMDECA) é um Fundo especial, criado por lei municipal, segundo determinação do Estatuto da Criança e do Adolescente em seu art. 260 com o objetivo de financiar programas e projetos que atuem na garantia da promoção, proteção e defesa dos direitos da criança e do adolescente. 
+          </h5>
+            <a href="nucleo-icons.html" class="btn btn-info" target="_blank">Saiba mas</a>
+
+          </div>
+        </div>
+
+
+        <hr>
+        <h2 class="title">Novidades</h2>
+        <div id="carouselExampleIndicators" class="carousel slide col-lg-5-10 col-12 m-2 p-5" data-ride="carousel" style="">
+          <ol class="carousel-indicators " >
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner"   >
+            <div class="carousel-item active">
+              <img src="assets/img/comdica3.jpg" style='' class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5 style=" ">primeira postagem</h5>
+                <p></p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="assets/img/comdica3.jpg" style='' class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>Segunda postagem</h5>
+                <p></p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="assets/img/comdica3.jpg" class="d-block w-100" alt="...">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>terceira</h5>
+                <p></p>
+              </div>
             </div>
           </div>
-       
-      
-      <hr>
-  <div id="carouselExampleIndicators" class="carousel slide col-lg-5-10 position-relative p-5" data-ride="carousel" style="">
-    <ol class="carousel-indicators " >
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner"  style="margin-top: " >
-      <div class="carousel-item active">
-        <img src="assets/img/comdica3.jpg" style='' class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5 style=" ">primeira postagem</h5>
-          <p></p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="assets/img/comdica3.jpg" style='' class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Segunda postagem</h5>
-          <p></p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="assets/img/comdica3.jpg" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>terceira</h5>
-          <p></p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
- </div>
-</div>
-<!-- Optional JavaScript --><center>
-<div style="margin-top: 5em;">
- <div  class='row' style="float: right;">
-  <div class="col-md-3 col-sm-6">
-    <div class='report-module'>
-      <div class='thumbnail' >
-        <a href="view_visualizar_pontos.php?id=<?=$linha['id']?>">
-          <img class="card-img-top" style="max-height: 11em;" src="assets/img/comdica3.png">
-        </a>
-      </div>
-      <div class='post-content'>
-        <div class='category'></div>  
-        <h2 class='title' style="height: 2.5em;">Comdica Araçoiaba</h2>
-        <p class='description' style="height: 2em;">Comdica Araçoiaba está com um novo site para...</p>
-        <div class='post-meta'>
-          <span class='comments'>
-            <a class="btn btn-primary  btn-block" id="but" style="border:1px solid black;" href="view_visualizar_pontos.php?id=<?=$linha['id']?>">Saiba mas</a>
-          </span>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
       </div>
     </div>
   </div>
+  <!-- Optional JavaScript --><center>
+    <div style="margin-top: 5em;">
+     <div  class='row' style="float: right;">
+      <div class="col-md-3 col-sm-6">
+        <div class='report-module'>
+          <div class='thumbnail' >
+            <a href="view_visualizar_pontos.php?id=<?=$linha['id']?>">
+              <img class="card-img-top" style="max-height: 11em;" src="assets/img/comdica3.png">
+            </a>
+          </div>
+          <div class='post-content'>
+            <div class='category'></div>  
+            <h2 class='title' style="height: 2.5em;">Comdica Araçoiaba</h2>
+            <p class='description' style="height: 2em;">Comdica Araçoiaba está com um novo site para...</p>
+            <div class='post-meta'>
+              <span class='comments'>
+                <a class="btn btn-primary  btn-block" id="but" style="border:1px solid black;" href="view_visualizar_pontos.php?id=<?=$linha['id']?>">Saiba mas</a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
 
-  <div class="col-md-3 col-sm-6">
-    <div class='report-module' style="">
-      <div class='thumbnail' >
-        <a href="view_visualizar_pontos.php?id=<?=$linha['id']?>">
-          <img class="card-img-top" style="max-height: 11em;" src="assets/img/comdica3.png">
-        </a>
+      <div class="col-md-3 col-sm-6">
+        <div class='report-module' style="">
+          <div class='thumbnail' >
+            <a href="view_visualizar_pontos.php?id=<?=$linha['id']?>">
+              <img class="card-img-top" style="max-height: 11em;" src="assets/img/comdica3.png">
+            </a>
+          </div>
+          <div class='post-content'>
+            <div class='category'></div>  
+            <h2 class='title' style="height: 2.5em;">Comdica Araçoiaba abre novo chamado...</h2>
+            <p class='description' style="height: 2em;">Comdica Araçoiaba está com um novo site para...</p>
+            <div class='post-meta'>
+              <span class='comments'>
+                <a class="btn btn-primary  btn-block" id="but" style="border:1px solid black;" href="view_visualizar_pontos.php?id=<?=$linha['id']?>">Saiba mas</a>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class='post-content'>
-        <div class='category'></div>  
-        <h2 class='title' style="height: 2.5em;">Comdica Araçoiaba abre novo chamado...</h2>
-        <p class='description' style="height: 2em;">Comdica Araçoiaba está com um novo site para...</p>
-        <div class='post-meta'>
-          <span class='comments'>
-            <a class="btn btn-primary  btn-block" id="but" style="border:1px solid black;" href="view_visualizar_pontos.php?id=<?=$linha['id']?>">Saiba mas</a>
-          </span>
+      <div class="col-md-3 col-sm-6">
+        <div class='report-module' >
+          <div class='thumbnail' >
+            <a href="view_visualizar_pontos.php?id=<?=$linha['id']?>">
+              <img class="card-img-top" style="max-height: 11em;" src="assets/img/comdica3.png">
+            </a>
+          </div>
+          <div class='post-content'>
+            <div class='category'></div>  
+            <h2 class='title' style="height: 2.5em;">Jovens criam projeto de doaçoes  </h2>
+            <p class='description' style="height: 2em;">Comdica Araçoiaba está com um novo site para...</p>
+            <div class='post-meta'>
+              <span class='comments'>
+                <a class="btn btn-primary  btn-block" id="but" style="border:1px solid black;" href="view_visualizar_pontos.php?id=<?=$linha['id']?>">Saiba mas</a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <div class='report-module' >
+          <div class='thumbnail' >
+            <a href="view_visualizar_pontos.php?id=<?=$linha['id']?>">
+              <img class="card-img-top" style="max-height: 11em;" src="assets/img/comdica3.png">
+            </a>
+          </div>
+          <div class='post-content'>
+            <div class='category'></div>  
+            <h2 class='title' style="height: 2.5em;">Faça uma doação</h2>
+            <p class='description' style="height: 2em;">Comdica Araçoiaba está com um novo site para...</p>
+            <div class='post-meta'>
+              <span class='comments'>
+                <a class="btn btn-primary  btn-block" id="but" style="border:1px solid black;" href="view_visualizar_pontos.php?id=<?=$linha['id']?>">Saiba mas</a>
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-  <div class="col-md-3 col-sm-6">
-    <div class='report-module' >
-      <div class='thumbnail' >
-        <a href="view_visualizar_pontos.php?id=<?=$linha['id']?>">
-          <img class="card-img-top" style="max-height: 11em;" src="assets/img/comdica3.png">
-        </a>
-      </div>
-      <div class='post-content'>
-        <div class='category'></div>  
-        <h2 class='title' style="height: 2.5em;">Jovens criam projeto de doaçoes  </h2>
-        <p class='description' style="height: 2em;">Comdica Araçoiaba está com um novo site para...</p>
-        <div class='post-meta'>
-          <span class='comments'>
-            <a class="btn btn-primary  btn-block" id="but" style="border:1px solid black;" href="view_visualizar_pontos.php?id=<?=$linha['id']?>">Saiba mas</a>
-          </span>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-3 col-sm-6">
-    <div class='report-module' >
-      <div class='thumbnail' >
-        <a href="view_visualizar_pontos.php?id=<?=$linha['id']?>">
-          <img class="card-img-top" style="max-height: 11em;" src="assets/img/comdica3.png">
-        </a>
-      </div>
-      <div class='post-content'>
-        <div class='category'></div>  
-        <h2 class='title' style="height: 2.5em;">Faça uma doação</h2>
-        <p class='description' style="height: 2em;">Comdica Araçoiaba está com um novo site para...</p>
-        <div class='post-meta'>
-          <span class='comments'>
-            <a class="btn btn-primary  btn-block" id="but" style="border:1px solid black;" href="view_visualizar_pontos.php?id=<?=$linha['id']?>">Saiba mas</a>
-          </span>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
 
 </center>
 
@@ -172,7 +178,7 @@
 </body>
 
 <?php 
-    include 'footer.php';
- ?>
- 
+include 'footer.php';
+?>
+
 </html>
