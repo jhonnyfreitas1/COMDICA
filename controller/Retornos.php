c19646faec31d2d092702dcada889077f704fbe1
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-04-16 12:50:10 -0300

    adicionando funções de retorno do dashboard do usuario

--- controller/Retornos.php
+++ controller/Retornos.php
@@ -1,4 +1,16 @@
-<?php
-return  include '../nova-postagem.php';
+<?php  session_start();
+		include 'bd-conection.php';
+		$quero = htmlspecialchars($_POST['quero']."();");
+		return eval($quero);
+					 
+		
+		 function postagem(){
+		 	islogged();
+			 return  include '../nova-postagem.php';		
+			}	
 
-?>
+			function islogged(){
+				if (!$_SESSION['name'] && !$_SESSION['id_user']){
+					echo  '<script>alert("não estás logado!"); window.location.assign("../index.php");</script>';
+				}
+			}
