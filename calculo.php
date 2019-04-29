bcc4e871a8ed76cc0863a6853aec8484ec094ee6
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-04-29 13:20:58 -0300

    modificando pagina de calculo para receber script js

--- calculo.php
+++ calculo.php
@@ -14,11 +14,14 @@
     color:#212529;
   }
 </style>
-
-  <!-- Bootstrap CSS -->
+ <meta name="viewport" content="width=device-width, initial-scale=1">    
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+  <script type="text/javascript" src="assets/js/jquery.maskMoney.min.js"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="assets/css/calc.css">
-
+ 
+ <script src="assets/js/calculo.js"></script>
   <title>Cálculo do imposto de renda</title>
 </head>
 
@@ -32,12 +35,12 @@
   <div class="inputCalc" style="margin-top: 2%;">
   <div class="form-row" >
     <div class="form-group col-md-6" >
-      <label for="inputEmail4" >Renda bruta</label>
-      <input type="number" class="form-control radius" id="inputEmail4" placeholder="R$ 0,00">
+      <label for="inputEmail4"  >Renda bruta</label>
+      <input type="text" data-affixes-stay="true" data-prefix="R$ " data-thousands="." data-decimal="," class="form-control radius" id="renda-bruta" placeholder="R$ 0,00">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4">Número de dependentes</label>
-      <input type="number" class="form-control radius" id="inputPassword4" placeholder="0">
+      <input type="text" class="form-control radius" maxlength="3" size="4" id="dependentes" placeholder="1,2,3..etc" >
     </div>
   </div>
 </div>
@@ -45,33 +48,32 @@
 <div class="form-row inputCalc">
     <div class="form-group col-md-6" >
       <label for="inputEmail4">Despesa anual com ensino</label>
-      <input type="number" class="form-control radius" id="inputEmail4" placeholder="R$ 0,00">
+      <input type="text" data-affixes-stay="true" data-prefix="R$ " data-thousands="." data-decimal="," class="form-control radius" id="desp-ensino" placeholder="R$ 0,00">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4">Despesa anual com pensão alimentícia</label>
-      <input type="number" class="form-control radius" id="inputPassword4" placeholder="R$ 0,00">
+      <input type="text" data-affixes-stay="true" data-prefix="R$ " data-thousands="." data-decimal="," class="form-control radius" id="desp-alim" placeholder="R$ 0,00">
     </div>
   </div>
 
 <div class="form-row inputCalc" style="margin-bottom: 1%">
     <div class="form-group col-md-6" >
       <label for="inputEmail4">Despesa anual médica</label>
-      <input type="number" class="form-control radius" id="inputEmail4" placeholder="R$ 0,00">
+      <input type="text" data-affixes-stay="true" data-prefix="R$ " data-thousands="." data-decimal="," class="form-control radius" id="desp-medic" placeholder="R$ 0,00">
     </div>
     <div class="form-group col-md-6">
       <label for="inputPassword4">Previdência social (INSS)</label>
-      <input type="number" class="form-control radius" id="inputPassword4" placeholder="R$ 0,00">
+      <input type="text" data-affixes-stay="true" data-prefix="R$ " data-thousands="." data-decimal="," class="form-control radius" id="inss" placeholder="R$ 0,00">
     </div>
   </div>
 
- <button type="submit" class="btn btn-success" style="width: 10%; display: block; margin: auto;">Calcular</button>
+ <button type="submit" class="btn btn-success" id='calcular' style="width: 10%; display: block; margin: auto;">Calcular</button>
 
 </form>
 
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- jQuery first, then Popperjs, then Bootstrap JS -->
+
+
 <style type="text/css">
   .centered {
     margin: 0 auto !important;
@@ -84,7 +86,7 @@
 
 </style>
 </body>
-
+ 
 <?php 
     include 'footer.php';
 ?>
