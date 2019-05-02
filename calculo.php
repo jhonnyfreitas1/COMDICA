128b0cc606a7c7663e62c21dd0ac2a587efdf3d9
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-05-01 23:02:56 -0300

    adicionando divs para mostrar os resultados do  calculo

--- calculo.php
+++ calculo.php
@@ -35,7 +35,7 @@
   <div class="inputCalc" style="margin-top: 2%;">
   <div class="form-row" >
     <div class="form-group col-md-6" >
-      <label for="inputEmail4"  >Renda bruta</label>
+      <label for="inputEmail4"  >Renda mensal bruta</label>
       <input type="text" data-affixes-stay="true" data-prefix="R$ " data-thousands="." data-decimal="," class="form-control radius" id="renda-bruta" placeholder="R$ 0,00">
     </div>
     <div class="form-group col-md-6">
@@ -72,27 +72,34 @@
 </form>
 
 <!-- jQuery first, then Popperjs, then Bootstrap JS -->
+<div class="row col-md-12 m-2" style=""  id='resultados' >
+<div   class="alert alert-warning col-md-6" role="alert">
 
-<div style="display:none;" class="alert alert-success row" role="alert">
- <div>
   <h4 class="alert-heading">Antes de doar</h4>
   <p id="seuinss">Inss: <b>201111</b></p>
   <p id="suarendaanual">Renda anual:<b> 240000</b></p>
   <p id="seusdebitos">Debitos da base:<b> 20000</b></p>
   <p id="basedecalculo">Base de calculo:<b> 200000</b></p>
    <p id="eliquota">Eliquota:<b> 7%</b></p>
+   <p id="impostoir">IR:<b> 7%</b></p>
   <hr>
-  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+  <p class="mb-0">O nosso simulador não deve ser substituido pela calculadora oficial da Receita da Fazenda</p>
 </div>
- <div>
-  <h4 class="alert-heading">Doando</h4>
-  <p id="seuinss">Inss: <b>201111</b></p>
-  <p id="suarendaanual">Renda anual:<b> 240000</b></p>
-  <p id="seusdebitos">Debitos da base:<b> 20000</b></p>
-  <p id="basedecalculo">Base de calculo:<b> 200000</b></p>
-   <p id="eliquota">Eliquota:<b> 7%</b></p>
+
+<div    class="alert alert-success col-md-6 " role="alert">
+  <h4 class="alert-heading">Doando 6%</h4>
+  <p id="seuinss2">Inss: <b>201111</b></p>
+  <p id="suarendaanual2">Renda anual:<b> 240000</b></p>
+  <p id="seusdebitos2">Debitos da base:<b> 20000</b></p>
+  <p id="basedecalculo2">Base de calculo:<b> 200000</b></p>
+   <p id="eliquota2">Eliquota:<b> 7%</b></p>
+   <p id="impostoir2">IR:<b> 7%</b></p>
+   <p id="valor7"></p>
+   <button class="btn-success" id="doar"></button>
   <hr>
-  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+  <p class="mb-0">Alguma informação que seja de grande importancia.</p>
+
+</div>
 </div>
 </div>
 <style type="text/css">
