daa0e3c140a50d21a5ca13062157c34c68c11f28
RenissonSilva <renissonx@gmail.com>
2019-05-31 16:18:34 -0300

    Criação pagina pq doar

---
+++ pq_doar.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  
+<style type="text/css">
+  @font-face {
+  font-family: Lovelo_Black;
+  src: url(assets/fonts/Lovelo_Black.otf);
+}
+  a{
+    color:#212529;
+    font-weight: bolder;
+  }
+  a:hover{
+    color:#212529;
+  }
+  .pq_doar{
+    background-image: url('assets/img/fundo_criancas.jpg');
+    background-repeat: no-repeat;
+    background-size: 100%;
+    opacity: 0.5;
+  }
+  .texto_doacao{
+    color:white;
+    padding: 15%;
+    font-family: Lovelo_Black;
+    text-shadow: 2px 2px #000;
+  }
+
+</style>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <title>Cálculo do imposto de renda</title>
+</head>
+
+<?php 
+  include 'nav.php';
+ ?>
+
+<body>
+
+<div class="pq_doar" style="height: 500px">
+<h5 class="texto_doacao">
+  Sua doação faz a diferença, e ela pode ser feita a partir da destinação de um percentual do imposto que você paga , sendo redirecionada à ajuda de crianças carentes de araçoiaba , tudo isso sem pagar à mais , pois o dinheiro já seria recolhido pela receita federal.
+Pessoas físicas podem deduzir até 6% do imposto de renda, para verificar quanto você pode destinar para a fundação , basta simular na calculadora, ela pode ser acessada por aqui.
+Realizando a contribuiçao você estará mudando o destino de diversas crianças araçoiabenses.
+</h5>
+</div>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<style type="text/css">
+  .centered {
+    margin: 0 auto !important;
+    float: none !important;
+
+  }
+  h5{
+    color: black;
+  } 
+
+</style>
+</body>
+
+<?php 
+    include 'footer.php';
+?>
+
+</html>
