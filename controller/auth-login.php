8cd960f0a0792c9b6c57a50d67cb04823f6ccd91
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-06-03 12:57:20 -0300

    adicionando  forma de autenticar usuario com senha criptografadas

--- controller/auth-login.php
+++ controller/auth-login.php
@@ -4,19 +4,23 @@
 	$username = htmlspecialchars($_POST['user_email']);
 	$password = htmlspecialchars($_POST['password']);
 
-	$consulta = $conn -> prepare('SELECT id_user,name,admin  FROM users where email=?  and password=?');
+
+	$consulta = $conn -> prepare('SELECT id_user,name,admin,password  FROM users where email=?');
 	$consulta -> bindValue(1,$username);
-	$consulta ->bindValue(2,$password);
 	$consulta ->execute();
 	$response = $consulta->fetch();
 				 	
 		if ($response == null) {
 			header("location:../login-admin.php?error=erro_dados");
 		}else{
-			session_start();
-			$_SESSION['id_user'] = $response['id_user'];
-			$_SESSION['name'] = $response['name'];
-			$_SESSION['user-admin'] = $response['admin'];
 
-			header('location:../dashboard-admin.php');
+			if (password_verify($password, $response['password'])) {
+		   		session_start();
+				$_SESSION['id_user'] = $response['id_user'];
+				$_SESSION['name'] = $response['name'];
+				$_SESSION['user-admin'] = $response['admin'];
+				header('location:../dashboard-admin.php');
+		} else {
+		   header("location:../login-admin.php?error=erro_dados");
+		}
 			}
