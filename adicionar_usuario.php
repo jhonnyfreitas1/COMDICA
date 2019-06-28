9d7fb9f57de4bbb7d6b427c1b326b80855f95b62
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-06-28 02:07:58 -0300

    #10 modificando pagina de adicionar usuario admin

--- adicionar_usuario.php
+++ adicionar_usuario.php
@@ -1,34 +1,39 @@
-   <form method="post" action="" class="form-signin" enctype="multipart/form-data">
-            
-                            <div class="form-label-group">
-                                <label for="nome" class="text-center">Nome Completo * </label>
-                                <input id="nome" class="form-control " type="text" name="user_name" placeholder="a" required>
+         
+                        <div class="container mt-4" style=" ">
+                    <form method="post" action="controller/action_create_user.php" class="form-signin ">
+                                <h3>Cadastre um novo Administrador no site</h3>
+                            <div class="form-label-group col-md-12">
+                                <label for="nome" class="text-center">Nome Completo<span style="color: red;">*</span> </label>
+                                <input id="nome" class="form-control" data-toggle="tooltip" data-placement="left" title="Esse nome não vai ser seu identificador, apenas será para contribuir mas com suas informações" type="text" name="user_name" placeholder="Ex: Maria francisca..." required>
                         
                             </div>
-                            <div class="row">
-                                <div class="form-label-group col-md-6">
-                                    <label for="inputMatricula" class="text-center">Função</label>
-                                    <input id="inputMatricula" class="form-control" type="text" name="user_siap_matricula" placeholder="a" value="" maxlength="14" required>
-                                  
-                                </div>
-                            </div>
-                            <div class="form-label-group">
-                                <label for="email" class="text-center">E-mail *</label>
-                                <input id="email" class="form-control" type="email" name="user_email" placeholder="a" value="" required>
+                       
+                            <div class="form-label-group col-md-12 ">
+                                <label for="email" class="text-center">E-mail<span style="color: red;">*</span></label>
+                                <input id="email" class="form-control" data-toggle="tooltip" data-placement="left" title="Utilize um e-mail valído pós você receberá um e-mail para a confirmação do mesmo" type="email" name="user_email" placeholder="Ex: Example@example.com"  required>
                               
                             </div>
-                            <div class="row">
-                                <div class="form-label-group col-md-6 ">
-                                    <label class="text-center" for="senha"> Senha *</label>
-                                    <input id="senha" class="form-control" type="password" name="password" required placeholder="a" maxlength="16" minlength="8">
-                                    
+                        
+                                <div class="form-label-group col-md-12 ">
+                                    <label class="text-center" for="senha"> Senha<span style="color: red;">*</span></label>
+                                    <input id="senha" class="form-control" type="password" name="password" required placeholder="**********"  minlength="8">  
                                 </div>
 
-                                <div class="form-label-group col-md-6">
-                                    <label class="text-center" for="senha2"> Repita a Senha *</label> 
-                                    <input id="senha2" placeholder="a" class="form-control" type="password" name="password2" maxlength="16" minlength="8" required>
-
-                            <button class="btn btn-success col-md-12  text-uppercase" id="mudar" type="submit">
+                                <div class="form-label-group col-md-12">
+                                    <label class="text-center" for="senha2"> Repita a Senha<span style="color: red;">*</span></label> 
+                                    <input id="senha2" placeholder="**********" class="form-control" type="password" name="password2"  minlength="8" required>
+                            <button class="btn btn-success col-md-12 m-2 text-uppercase" id="mudar" type="submit">
                                 Cadastrar
                             </button>
-                        </form
+                        </form>
+                        </div>
+                        </center>
+
+                        <style type="text/css">
+                            body{
+                                background: linear-gradient(135deg, rgb(34, 130, 227) 0%, rgb(9, 226, 51) 100%);
+                            }
+                        </style>
+                        <script type="text/javascript">
+                        
+                        </script>
