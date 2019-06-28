74a2466f65ba05a79323c1cc577f4e31abd1cfcf
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-06-28 02:10:33 -0300

    #9 retornos da acao de adicionar usuario relatar erros e acertos no cadastro da postagem

--- controller/action-post.php
+++ controller/action-post.php
@@ -23,17 +23,11 @@ function is_exists($titulo, $conn){ //verifica se o poste já existe
 		$hidden=filter_var($_POST['hidden']);
 		$descricao = $_POST['descricao'];
 		$imagem = $_FILES['imagem']; //arquivo enviado
-		
+		$pdf2 = $_FILES['pdf2'];
+		$pdf = $_FILES['pdf'];
 		if ($nome == "" || $descricao == "" || $imagem == "") {
  		
- 			header("location:../dashboard-admin.php?sucess=dados_not_found");
- 	
-		}
-		if ($_FILES['pdf']['name']) {
-			$pdf = $_FILES['pdf'];
-		}
-		if ($_FILES['pdf2']['name']) {
-			$pdf2 = $_FILES['pdf2'];
+ 			echo 'Descricao, titulo da postagem ou a imagem principal nao foram selecionadas';
 		}
 		
 		$yt = $_POST['yt'];
@@ -49,23 +43,25 @@ function is_exists($titulo, $conn){ //verifica se o poste já existe
 			$novo_nome =  "imagem_".md5(time()).$extensao;//nome do arquivo salvo
 			$diretorio =  "../upload/";//onde ele será salvo
 			move_uploaded_file($imagem['tmp_name'], $diretorio.$novo_nome);//mover o arquivo para o diretorio
+
 		}	
 
 		if(isset($pdf)){
 			$extensao1  =  strtolower(substr($pdf['name'], -4));
-			$nome_pdf =  "pdf_".md5(time()).$extençoes1;
+			$nome_pdf 	=  "pdf_".md5(time()).$extensao1;
 			$diretorio1 =  "../upload/";
 			move_uploaded_file($pdf['tmp_name'], $diretorio1.$nome_pdf);
+		
 		}
+
 		if(isset($pdf2)){
 			$extensao2  =  strtolower(substr($pdf2['name'], -4));
-			$nome_pdf2 =  "pdf_".md5($pdf2['name']).time().$extençoes2;
+			$nome_pdf2 	=  "pdf_".md5($pdf2['name']).time().$extensao2;
 			$diretorio2 =  "../upload/";
 			move_uploaded_file($pdf2['tmp_name'], $diretorio2.$nome_pdf2);
 		}
-
- 		$sql = "INSERT INTO posts(user_id, nome_post, imagem,descricao_post,link_yt,pdf,pdf2) 
-						VALUES(?,?,?,?,?,?,?)";
+ 		if (is_exists($nome , $conn )) {
+ 		$sql = "INSERT INTO posts(user_id, nome_post, imagem,descricao_post,link_yt,pdf,pdf2) VALUES(?,?,?,?,?,?,?)";
  		$query = $conn->prepare($sql);
 		$query->bindParam(1, $id);
  		$query->bindParam(2, $nome);
@@ -75,9 +71,12 @@ function is_exists($titulo, $conn){ //verifica se o poste já existe
 		$query->bindParam(6, $nome_pdf);
 		$query->bindParam(7, $nome_pdf2);
  		$stmt = $query->execute();
-		header("location:../dashboard-admin.php?sucess=sim");
-
+		echo "sucesso ao fazer a nova postagem";
+ 		}else{
+ 			echo 'Uma postagem com esse título já existe, tente novamente';
+		}
 	} else {
- 		header("location:../dashboard-admin.php?sucess=erro");
+ 		echo "Erro ao fazer a postagem, tente novamente";
  	}
+	
 	
