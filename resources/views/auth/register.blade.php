161960e2b922414eb4797932e69b09c14f2eb63b
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-07-11 21:22:59 -0300

    tentando colocar alert de sucesso em adicionar um usuario sem sucesso

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+   @if(!empty($message))
+
+   <div class="alert alert-success float-right col-md-3">
+            <p class="">{{$message}}</p>
+   </div>
+
+   @endif
+<div class="container mb-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card shadow-lg">
@@ -12,7 +20,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
+                            <label for="name" class="text-dark col-md-4 col-form-label text-md-right">{{ __('Nome') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -26,7 +34,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Endereço de e-mail') }}</label>
+                            <label for="email" class="text-dark col-md-4 col-form-label text-md-right">{{ __('Endereço de e-mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -40,7 +48,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
+                            <label for="password" class="text-dark col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -54,7 +62,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirme a senha') }}</label>
+                            <label for="password-confirm" class="text-dark col-md-4 col-form-label text-md-right">{{ __('Confirme a senha') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -74,6 +82,6 @@
             </div>
         </div>
     </div>
+    <a href="/admin/comdica" class="btn btn-info mt-4 float-right mr-5 mb-4 ">Voltar para área do adminstrador</a>
 </div>
-    <a href="/admin/comdica" class="btn btn-info float-right mr-5 ">Voltar para área do adminstrador</a>
 @endsection
