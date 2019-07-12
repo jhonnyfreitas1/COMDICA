fea502e90ca0791d9f5932d67299763a7546c78b
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-07-11 21:27:40 -0300

    #37  criando rota de recebimento de callback do gerencianet

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/boleto/notification', ['as' => 'gerencianet.gerar_carne', 'uses' =>'GerenciaNetController@notification']);
