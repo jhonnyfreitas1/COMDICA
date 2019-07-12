e97d6bf71c1e165869e04158a01ecd0ce0fc4996
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-07-11 21:31:36 -0300

    #37  criando migrations de tokens que vao ser enviados do gerencianet

---
+++ database/migrations/2019_07_11_024532_tokens.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Tokens extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens');
+    }
+}
