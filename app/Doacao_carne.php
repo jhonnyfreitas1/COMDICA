c2c14768f4fca90de61c18dcce031eff717559d7
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-09-01 22:15:05 -0300

    #38 criando model de doacao carne

---
+++ app/Doacao_carne.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Relations\hasMany;
+use Illuminate\Database\Eloquent\Model;
+use App\Doacao_boleto;
+class Doacao_carne extends Model
+{
+    protected $table = "doacao_carne";
+    
+    protected $primaryKey = 'carne_id';
+
+    protected $fillable = [
+    	'doador_nome','link_carne',
+    ];
+    
+    protected $guarded =[
+    	'valor_parcelado','valor_total', 'parcelas_pagas',
+    ];
+ 	public $timestamps = true;
+}
+     public function carne_id(){
+        return $this->hasMany(Doacao_boleto::class);
+    }
