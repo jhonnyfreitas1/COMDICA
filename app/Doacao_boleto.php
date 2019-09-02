9a29bbe97545be3b4a3e9ed0c57c11bdd94c4db1
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-09-01 22:16:16 -0300

    #38 criando model de doacao_boleto

--- app/Doacao_boleto.php
+++ app/Doacao_boleto.php
@@ -1,13 +1,28 @@
 <?php
 
 namespace App;
-
+use App\Doacao_carne;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
 
 class Doacao_boleto extends Model
 {
-    
+    protected $table = "doacao_boleto";
 
-
+    protected $primaryKey = 'id';
     
+    public $timestamps = true;
+
+    public $fillable = [
+    	'status','doador_nome','doador_email','doador_telefone',
+    ];
+    protected $guarded = [
+    	 'valor_total','metodo_pagamento','cod_barra','fk_id_carne',
+    ];	
+
+	  public function fk_id_carne(){
+         return $this->belongsTo(Doacao_carne::class);
+     }     
+
 }
