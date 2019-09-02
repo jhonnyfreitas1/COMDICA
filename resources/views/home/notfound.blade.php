7f39b7deb8e7c6e6af2a07bf1554c4802121bd21
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-09-01 22:38:49 -0300

    #45 criando pagina de 404

---
+++ resources/views/home/notfound.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+	@section('content')
+		<div class=" col-md-12 container" style="">
+			<h1 class="danger  text-light bg-dark">Oppss... Conteúdo não encontrado. 404</h1>
+		</div>
+
+		<style type="text/css">
+			
+			body{
+				background-image: url('img/cmd-preto.png');
+				background-repeat: no-repeat;
+				size: 110%;
+				background-position: 50% 10%;
+			}	
+
+		</style>
+	@endsection
