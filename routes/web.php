6f8bcf6695d9c58cc64e67291ac781bc483cef20
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-09-01 23:01:22 -0300

    #45  #30 criando rotas referentes a contato e  pagina de 404

--- routes/web.php
+++ routes/web.php
@@ -26,17 +26,22 @@ Route::get('/calculadora/cancelar_transacao/{id}', ['as' => 'gerencianet.cancela
 
 //Rotas do site 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/notfound', 'HomeController@notfound')->name('home');
 Route::get('/home1', 'HomeController@home1')->name('home1');
 Route::get('/postagem/{id}', ['as' => 'postagem', 'uses' => 'HomeController@postagem']);
 Route::get('/porque_doar', ['as' => 'porque_doar', 'uses' => 'HomeController@pq_doar']);
 Route::get('/contato', ['as' => 'contato', 'uses' => 'HomeController@contato']);
 Route::post('/status', ['as' => 'status', 'uses' => 'HomeController@status']);
+Route::post('/contato/save', ['as' => 'home.contato', 'uses' => 'HomeController@create_contato']);
 
 
 //Rotas admin
 Route::get('/admin/comdica', ['as' => 'admin', 'uses' => 'AdminController@index'])->middleware('auth');
+Route::get('/admin/contato', ['as' => 'admin.contato', 'uses' => 'AdminController@contato'])->middleware('auth');
+Route::get('/admin/contato/{id}', ['as' => 'admin.contato.id', 'uses' => 'AdminController@contato_single'])->middleware('auth');
 Route::get('/admin/doacoes', ['as' => 'admin.boleto', 'uses' => 'AdminController@doacoes_boleto'])->middleware('auth');
-Route::get('/admin/minhas_postagens', ['as' => 'minhas_postagens', 'uses' => 'AdminController@minhas_postagens'])->middleware('auth');
+Route::get('/admin/back', ['as' => 'admin.back', 'uses' => 'AdminController@back'])->middleware('auth');
+Route::get('/admin/minhas_postagens', ['as' => 'admin.minhas_postagens', 'uses' => 'AdminController@minhas_postagens'])->middleware('auth');
 Route::get('/auth/logout', 'Auth\LoginController@logout')->middleware('auth');
 Route::get('/admin/post/delete/{id}', ['as' => 'delete_postagem', 'uses' => 'AdminController@destroy'])->middleware('auth');
 Route::get('/admin/nova-postagem', ['as' => 'admin.nova', 'uses' => 'AdminController@nova_postagem'])->middleware('auth');
