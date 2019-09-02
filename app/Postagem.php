dad56505f25ca2b920e67dac8fb0845110ca8aa9
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-09-01 22:15:23 -0300

    #38 criando model de postagem

---
+++ app/Postagem.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+use Illuminate\Database\Eloquent\Relations\belongsTo;
+class Postagem extends Model
+{
+      public $timestamps = true;
+      
+      protected $table = 'postagens';
+
+      protected $fillable = [
+            'titulo','descricao','imagem_principal','link_yt','pdf1','pdf2',
+      ];
+    
+      protected $guarded = [
+            'user_id',
+      ];
+
+      public function user_id(){
+      	return $this->belongsTo(User::class);
+      }
+
+ }
