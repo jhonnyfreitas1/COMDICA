344fc99f97168d7d382f314eab47d62b7e287835
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-09-30 03:17:39 -0300

    #53 layout modify'

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,7 +31,7 @@ class AdminController extends Controller
     public function nova_postagem()
     {
          $contato = Contato::where('visto', false)->get()->count();
-        return view('admin.nova_postagem')->with(compact('contato'));
+            return view('admin.nova_postagem')->with(compact('contato' , 'postagem'));
     }
 
      public function doacoes_boleto()
@@ -181,7 +181,8 @@ class AdminController extends Controller
     }
     public function edit($id)
     {
-        //
+        $postagem = Postagem::find($id);
+        return view('admin.edit_postagem')->with(compact('postagem'));
     }
     public function update()
     {
@@ -189,6 +190,78 @@ class AdminController extends Controller
 
        return view('admin.admin_update')->with(compact('user'));
     }
+    public function update_save(Request $request, $id){
+
+        Postagem::find($id);
+
+
+        if (strlen($request->titulo) > 50)
+        {
+            return 'O titulo de sua postagem não pode ultrapassar 50 caracteres';
+        }
+        $validPost = Postagem::where('titulo',$request->titulo)->first();
+    
+        if ($validPost){
+            return 'Uma postagem com esse titulo já existe';
+        }
+        $data =  date('Y-m-d H:i:s');
+            if ($request->hasFile('imagem') && $request->file('imagem')->isValid()) {   
+                    $imagem = $request->file()['imagem'];
+                    $numero = rand(11111,99999).$data;
+                    $dir = "upload_imagem";
+                    $ex = strtolower(substr($request->imagem, -4));
+                    $nomeImagem = "imagem_".$numero.".".$ex;
+                    $imagem->move($dir,$nomeImagem);
+                    $request->file()['imagem'] = $nomeImagem;
+                }
+
+                if ($request->hasFile('pdf') && $request->file('pdf')->isValid()) {   
+                    $pdf1 = $request->file()['pdf'];
+                    $numero = rand(11111,99999).$data;
+                    $diretorio = "upload_pdf";
+                    $ex = strtolower(substr($request->pdf, -4));
+                    $nomepdf = "pdf_".$numero.".pdf";
+                    $pdf1->move($diretorio,$nomepdf);
+                    $request->file()['pdf'] = $nomepdf;
+                }else{
+                    $nomepdf = null;
+                }
+                if ($request->hasFile('pdf2') && $request->file('pdf2')->isValid()) {   
+                    $pdf2 = $request->file()['pdf2'];
+                    $numero1 = md5(rand(11111,99999).$data);
+                    $diretorio = "upload_pdf";
+                    $nomepdf2 = "pdf_".$numero1.".pdf"; 
+                    $pdf2->move($diretorio,$nomepdf2);
+                    $request->file()['pdf2'] = $nomepdf2;
+                }else{
+                    $nomepdf2 = null;
+                }
+                if ($request['yt'] != "") {
+                    $url = explode("watch?v=", $request['yt']);
+                    $embed = $url[0]."embed/".$url[1];
+                }else{
+                    $embed = null;
+                }
+                    $categorianumber= intval($request['categoria']);
+                   $model = new Postagem;
+                   $model->titulo = $request['titulo'];
+                   $model->descricao = $request['descricao'];
+                   $model->link_yt = $embed;
+                   $model->imagem_principal = $nomeImagem;
+                   $model->categoria = $categorianumber;
+                   $model->pdf1 = $nomepdf;
+                   $model->pdf2 = $nomepdf2;
+                   $model->user_id = Auth::id();
+                  $resultado = $model->save();
+
+        if ($resultado == true) {
+            return 'Postagem feita com sucesso';
+
+        }else {
+            return 'Falha ao fazer postagem, tente novamente.';
+        }
+    }
+    
 
     public function destroy($id)
     {   
