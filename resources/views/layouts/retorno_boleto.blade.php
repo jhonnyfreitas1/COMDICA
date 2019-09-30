4fc5ee9ec75014c62c23043194ce588053f6cc1e
jhonnyfreitas1 <jhonnyfarias87@gmail.com>
2019-09-30 03:35:18 -0300

    #53 adicionando view de retorno para emissao de boletos

---
+++ resources/views/layouts/retorno_boleto.blade.php
@@ -0,0 +1,19 @@
+	
+	<div class="card text-center">
+		  <div class="card-header bg-info"> 
+			<h1> Olá {{$nome}} !</h1>
+		  </div>
+		  <div class="card-body">
+		    <h5 class="card-title">Como voce gerou um boleto/carnê para doação só lhe resta pagar o mesmo com os seguintes dados.</h5>
+		    <i class="fas fa-hand-holding-usd fa-3x text-info"></i>
+		    <p class="card-text"><b>Código de barras: (1̣º boleto/parcela)</b> {{$cod}} <a href="https://comdica.site/sou_doador/">Veja aqui as intruções</a></p>
+
+		    <a href="{{$link_boleto}}#body" class="btn btn-primary">Veja seu boleto ou carnê  aqui </a>
+		    <h5 class="card-title">Foi enviado um e-mail com o mesmo boleto/carnê para o endereço de E-mail: <label class="text-muted">{{$email}}</label></h5>
+		  </div>
+		  <div class="card-footer bg-info text-muted">
+		   O Fundo da criança e do adolescente de Araçoiaba agradece a sua atenção</br>
+		   se desconhece esses dados contate-nos.
+		  </div>
+		</div>
+	
