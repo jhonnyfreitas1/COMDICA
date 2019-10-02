20061e98434b937bb965697f4eadbf1a4d63d82b
RenissonSilva <renissonx@gmail.com>
2019-10-01 23:14:25 -0300

    Modificação do texto da tela pq_doar

--- resources/views/home/pq_doar.blade.php
+++ resources/views/home/pq_doar.blade.php
@@ -9,8 +9,10 @@
 
 <h5 class="texto_doacao">
   <p class="titulo">Por que doar?</p>
-  Sua doação faz a diferença, e ela pode ser feita a partir da destinação de um percentual do imposto que você paga , sendo redirecionada à ajuda de crianças carentes de araçoiaba , tudo isso sem pagar à mais , pois o dinheiro já seria recolhido pela receita federal.
-Pessoas físicas podem deduzir até 6% do imposto de renda, para verificar quanto você pode destinar para a fundação , basta simular na calculadora, ela pode ser acessada <a href="calculo.php" class="link">por aqui.</a>
-Realizando a contribuiçao você estará mudando o destino de diversas crianças araçoiabenses.
+  Sua doação faz a diferença!<br><br>
+Ela pode ser feita a partir da destinação de um percentual de seu Imposto de Renda, sendo redirecionado aos Programas e Projetos que atenderão as crianças e adolescentes de Araçoiaba. <br><br>
+È um imposto que a sociedade decide se deixa para a Receita Federal ou destina para investimento em seu Município. Tudo isso sem pagar nada a mais, se você tem dinheiro a restituir (a receber) o valor vem integral ao que você tem direito e se tem dinheiro a pagar a Receita Federal, o valor será deduzido do total devido. <br><br>
+Pessoas físicas podem doar até 6% do imposto de renda, que deverá ser feito até o dia 31 de dezembro do ano da doação. Para verificar quanto você pode destinar para o FUNDECA, basta simular na calculadora, ela pode ser acessada <a href="calculo.php" class="link">por aqui.</a><br><br>
+Realizando a contribuição você estará mudando o destino de diversas crianças e adolescentes de Araçoiaba.
 </h5>
 @endsection
