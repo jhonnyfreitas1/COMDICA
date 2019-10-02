53ee110cebb5afc402f830072a51940dac215b26
RenissonSilva <40182898+RenissonSilva@users.noreply.github.com>
2019-10-01 23:18:30 -0300

    Ajuste no link 'pq_doar'

--- resources/views/home/pq_doar.blade.php
+++ resources/views/home/pq_doar.blade.php
@@ -12,7 +12,7 @@
   Sua doação faz a diferença!<br><br>
 Ela pode ser feita a partir da destinação de um percentual de seu Imposto de Renda, sendo redirecionado aos Programas e Projetos que atenderão as crianças e adolescentes de Araçoiaba. <br><br>
 È um imposto que a sociedade decide se deixa para a Receita Federal ou destina para investimento em seu Município. Tudo isso sem pagar nada a mais, se você tem dinheiro a restituir (a receber) o valor vem integral ao que você tem direito e se tem dinheiro a pagar a Receita Federal, o valor será deduzido do total devido. <br><br>
-Pessoas físicas podem doar até 6% do imposto de renda, que deverá ser feito até o dia 31 de dezembro do ano da doação. Para verificar quanto você pode destinar para o FUNDECA, basta simular na calculadora, ela pode ser acessada <a href="calculo.php" class="link">por aqui.</a><br><br>
+Pessoas físicas podem doar até 6% do imposto de renda, que deverá ser feito até o dia 31 de dezembro do ano da doação. Para verificar quanto você pode destinar para o FUNDECA, basta simular na calculadora, ela pode ser acessada<a href="calculadora" class="link"> por aqui.</a><br><br>
 Realizando a contribuição você estará mudando o destino de diversas crianças e adolescentes de Araçoiaba.
 </h5>
 @endsection
