4f15175021379922aab3e0b26e342f2a86077d35
Erickson Ferreira <erickson.rinho@hotmail.com>
2020-02-12 05:08:15 -0300

    Atualizando a tabela de usuario

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -4,19 +4,14 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
    public function run()
     {
-        //
         DB::table('users')->insert([
-        'name' => 'Leonardo Pessoa',
+        'name' => 'jhonny farias',
         'email' => 'comdica@aracoiaba.com',
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'tipo_user' => 1,
           ]);  
     
     }
