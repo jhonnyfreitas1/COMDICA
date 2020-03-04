56b3b67e21149670c5ab6ff4e57ddbb98788d394
Murielson <jhonsnoow32@gmail.com>
2020-03-04 15:39:41 -0300

    #69 adicionando alterações básicas de fonte na index nova. Adicionando Blade do portifolio

---
+++ resources/views/layouts/newIndex.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>Comdica - Araçoiaba PE</title>
+		<link rel="stylesheet" href="{{asset('css/normalize.css')}}">
+		<link rel="stylesheet" href="{{asset('css/reset.css')}}">
+		<link rel="stylesheet" href="{{asset('css/grid.css')}}">
+		<link rel="stylesheet" href="{{asset('css/style.css')}}">
+		<link rel="stylesheet" href="{{asset('css/produtos.css')}}">
+		<link rel="stylesheet" href="{{asset('css/responsivo.css')}}">
+	</head>
+	<body>
+		<header class="header">
+			<div class="container">
+				<a href="{{route('newIndex')}}" class="grid-4">
+					<img src="{{asset('img/comdica3.jpg')}}" alt="Comdica">
+				</a>
+				<nav class="grid-12 header_menu">
+					<ul>
+						<li><a href="">Sobre</a></li>
+						<li><a href="">Denunciar</a></li>
+						<li><a href="{{route('portifolio')}}">Portfólio</a></li>
+						<li><a href="">Contato</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+			@yield('content')
+		<br><br>
+		<footer>
+			<div class="footer">
+				<div class="container">
+
+					<div class="grid-8 footer_historia">
+						<h3>Área Restrita</h3>
+						<p>Comdica...</p>
+					</div>
+
+					<div class="grid-4 footer_contato">
+						<h3>Contato</h3>
+						<ul>
+							<li>- (81) 9999-999</li>
+							<li>- cmdcaaracoiabape@gmail.com</li>
+							<li>- Araçoiaba, n 10012 - PE</li>
+						</ul>
+					</div>
+
+					<div class="grid-4 footer_redes">
+						<h3>Redes Sociais</h3>
+						<ul>
+							<li><a href="http://facebook.com" target="_blank"><img src="img/redes-sociais/facebook.sv"></a></li>
+							<li><a href="http://instagram.com" target="_blank"><img src="img/redes-sociais/instagram.sv"></a></li>
+							<li><a href="http://twitter.com" target="_blank"><img src="img/redes-sociais/twitter.sv"></a></li>
+						</ul>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="copy">
+				<div class="container">
+					<p class="grid-16">Comdica 2020 - Todos os Direitos Reservados.</p>
+				</div>
+			</div>
+		</footer>
+
+	</body>
+</html>
---
+++ resources/views/newFront/portifolio.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.newIndex')
+
+@section('content')
+    <br><br><br><br>
+    <section class="introducao-interna interna_produtos">
+        <div class="container">
+            <h1 data-anime="400" class="fadeInDown">Entidades</h1>
+            <p data-anime="800" class="fadeInDown">Conheça o Trabalho das nossas entidades</p>
+        </div>
+    </section>
+    <section class="container produto_item">
+        <div class="grid-11">
+            <img src="{{asset('img/wallpaper.jpg')}}" alt="Bikcraft Esporte" class="black">
+            <h2>FUNDECA</h2>
+        </div>
+        <div class="grid-5 produto_icone"><img src="{{asset('img/COR1.png')}}"></div>
+        <div class="grid-8"><img src="{{asset('img/fundeca.png')}}" alt="Bikcraft Esporte"></div>
+        <div class="grid-8 produto_info">
+            <p style="color:black">Fundeca tem o objetivo de reunir fundos para financiar os projetos..</p>
+            <ul>
+                <li>(81) 9999-999</li>
+                <li>Araçoiaba, n 0000 - PE</li>
+                <li>Entidade@entidade.com</li>
+                <li>site.com.br</li>
+            </ul>
+        </div>
+    </section>
+@endsection
