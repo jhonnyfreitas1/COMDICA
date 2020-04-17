78284b4954116268f2214a54c4022413b86b7478
ericksonferreira <erickson.rinho@hotmail.com>
2020-04-17 15:35:36 -0300

    corrigindo o seeder de postagens

--- database/seeds/PostagemSeeder.php
+++ database/seeds/PostagemSeeder.php
@@ -10,7 +10,7 @@ class PostagemSeeder extends Seeder
         DB::table('postagens')->insert([
         	'titulo' => 'CORONAVÍRUS',
             'descricao' => 'CORONA ATACA O MUNDO INTEITO',
-            'imagem_principal' => '../img/fundo_criancas.jpg',
+            'imagem_principal' =>'',
             'arquivada' => 0,
             'categoria' => 1,
             'user_id' => 2,
@@ -19,7 +19,7 @@ class PostagemSeeder extends Seeder
         DB::table('postagens')->insert([
         	'titulo' => 'VESTIBULAR IFPE',
             'descricao' => 'INSCREVA-SE, VEJA SE ATENDE AO PERFIL E SOLICITE A ISENÇÃO DA TAXA ATÉ 24 DE OUTUBRO',
-            'imagem_principal' => '../img/fundo_criancas.jpg',
+            'imagem_principal' => '',
             'arquivada' => 1,
             'categoria' => 1,
             'user_id' => 1,
