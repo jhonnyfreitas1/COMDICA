6de2ae71c8b22af062f17a0fb0edc2ad93e073c6
ericksonferreira <erickson.rinho@hotmail.com>
2020-05-02 03:10:54 -0300

    modificando ImgAlbumGaleriaSeeder

--- database/seeds/ImgAlbumGaleriaSeeder.php
+++ database/seeds/ImgAlbumGaleriaSeeder.php
@@ -7,22 +7,22 @@ class ImgAlbumGaleriaSeeder extends Seeder
     public function run()
     {
         DB::table('img_album_galerias')->insert([
-        	'nome' => 'img1.jpg',
+        	'nome' => '',
             'album_id' => 1,
         ]);
 
         DB::table('img_album_galerias')->insert([
-        	'nome' => 'img2.jpg',
+        	'nome' => '',
             'album_id' => 1,
         ]);
 
         DB::table('img_album_galerias')->insert([
-        	'nome' => 'img1.jpg',
+        	'nome' => '',
             'album_id' => 2,
         ]);
 
         DB::table('img_album_galerias')->insert([
-        	'nome' => 'img2.jpg',
+        	'nome' => '',
             'album_id' => 2,
         ]);
     }
